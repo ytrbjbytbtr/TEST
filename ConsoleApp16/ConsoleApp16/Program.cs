3a3320dd5e6fa7805913f12a8bbc4da439e1e2ff
ytrbjbytbtr <52233543+ytrbjbytbtr@users.noreply.github.com>
2023-06-06 09:00:00 +0300

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+
+        namespace Practices
+    {
+
+        public class Calculator
+        {
+            public int Additional(int a, int b)
+            {
+                return a + b;
+            }
+
+            public int Subtraction(int a, int b)
+            {
+                return a - b;
+            }
+
+            public int Miltiplication(int a, int b)
+            {
+                return a * b;
+            }
+
+            public int Division(int a, int b)
+            {
+                return a / b;
+            }
+        }
+    }
+}
+
